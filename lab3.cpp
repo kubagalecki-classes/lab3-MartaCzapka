f4e8a521f8627c958feb98eaceeac5500493288d
MartaCzapka <01123769@pw.edu.pl>
2020-11-11 22:17:41 +0000

    zad4

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+using namespace std;
+class Figura
+{
+    double pole;
 
+public:
+    void   setP(double P) { pole = P; }
+    double getP() { return pole; }
+};
+class Kolo : public Figura
+{
+public:
+    Kolo(double r)
+    {
+        pole = 3.14 * r * r;
+        setP(pole);
+    }
+    double pole;
+};
+class Kwadrat : public Figura
+{
+public:
+    Kwadrat(double a)
+    {
+        pole = a * a;
+        setP(pole);
+    }
+    double pole;
+};
 int main()
 {
-    puts("Hello, World!");
+    Kolo    k{3};
+    Kwadrat kw{2};
+    double  poleKola = k.getP();
+    cout << poleKola << "\n";
+    double poleKwad = kw.getP();
+    cout << poleKwad << "\n";
 }
