6678a96f507cd170615715b5ccbe908d0ea2f9dd
MartaCzapka <01123769@pw.edu.pl>
2020-11-12 20:55:28 +0000

    4

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -12,7 +12,7 @@ unsigned int transportujFlota(unsigned int towar)
       do{
         int temp_towar = 0;
         Statek* stateczek();
-        temp_towar=stateczek.transportuj();
+        temp_towar=stateczek->transportuj();
         licznik_tow = licznik_tow + temp_towar;
         if(dynamic_cast<Zaglowiec>(stateczek))
         licznik_zagl++;
