fd914d548544ca6235d2dc3a571a1f697e531512
MartaCzapka <01123769@pw.edu.pl>
2020-11-12 20:31:52 +0000

    1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    if(towar !=0)
+    {
+      int licznik_tow=0;
+      int licznik_zagl=0;
+      while(licznik_tow < towar)
+      do{
+        int temp_towar = 0;
+        Statek stateczek = Stocznia();
+        temp_towar=stateczek.transportuj();
+        licznik_tow = licznik_tow + temp_towar;
+        if(dynamic_cast<Zaglowiec>(stateczek))
+        licznik_zagl++;
+      }
+      
+    }
     return 0;
 }
