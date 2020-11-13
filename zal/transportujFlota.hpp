fce3850a7472d4769312c0261b79064201489baa
MartaCzapka <01123769@pw.edu.pl>
2020-11-13 16:58:59 +0000

    28

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -12,14 +12,24 @@ unsigned int transportujFlota(unsigned int towar)
       {
         int temp_towar = 0;
         Stocznia sto{};
-        Statek* stateczek = sto();
-        temp_towar=stateczek->transportuj();
-        licznik_tow = licznik_tow + temp_towar;
-        if(dynamic_cast<Statek*>(stateczek))
+        Zaglowiec* stateczek_z = sto();
+        Tankowiec* stateczek_t = sto();
+        if(dynamic_cast<Zaglowiec*>(stateczek_z))
        {
+         temp_towar=stateczek_z->transportuj();
+         temp_towar=stateczek_z->transportuj();
+         licznik_tow = licznik_tow + temp_towar;
           zaglowce=zaglowce+1;
+          delete stateczek_z;
         }
-        delete stateczek;
+        if(dynamic_cast<Tankowiec*>(stateczek_t))
+       {
+         temp_towar=stateczek_t->transportuj();
+         temp_towar=stateczek_t->transportuj();
+         licznik_tow = licznik_tow + temp_towar;
+          delete stateczek_t;
+        }
+      
       }
       return zaglowce;
     }
